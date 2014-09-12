9138af15590ca67de135105dc11a08bdca1fca14
AntonsT <at646@scarletmail.rutgers.edu>
2014-09-11 21:10:44 -0400

    .

--- setup.py
+++ setup.py
@@ -19,6 +19,6 @@ setup(
     scripts=[],
     license='LICENSE.txt',
     description='Radical Pilot based Replica Exchange Simulations Module',
-    long_description=open('README.md').read()
+    long_description=open('README.md').read(),
     install_requires=['radical.pilot']
 )
