068fd36cb60ce7a88ffe2ff6a453c08ab5ccd1a0
Haoyuan Chen <haoyuan.chen@rutgers.edu>
2014-10-10 11:42:10 -0400

    we are done now!

--- examples/amber/amber_pattern_b_salt_concentration/launcher_amber_2.sh
+++ examples/amber/amber_pattern_b_salt_concentration/launcher_amber_2.sh
@@ -1,1 +1,1 @@
-RADICAL_PILOT_VERBOSE=debug python launch_simulation_pattern_b_amber.py --input='amber_input.json'
+RADICAL_PILOT_VERBOSE=info python launch_simulation_pattern_b_amber.py --input='amber_input.json'
