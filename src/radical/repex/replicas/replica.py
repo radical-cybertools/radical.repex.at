4444ad41f42daea5600fd26546493097a196fd16
AntonsT <at646@scarletmail.rutgers.edu>
2014-11-23 18:15:44 -0500

    2d example templating

--- src/radical/repex/replicas/replica.py
+++ src/radical/repex/replicas/replica.py
@@ -56,8 +56,6 @@ class Replica(object):
         self.stopped_run = -1
 
 
-
-
 #-----------------------------------------------------------------------------------------------------------------------------------
 
 class ReplicaSalt(object):
@@ -96,6 +94,59 @@ class ReplicaSalt(object):
         self.old_coor = ""
         self.old_vel = ""
         self.old_ext_system = "" 
+        self.old_path = ""
+        self.first_path = ""
+        self.swap = 0
+        self.cores = cores
+        self.stopped_run = -1
+
+
+
+#-----------------------------------------------------------------------------------------------------------------------------------
+
+class Replica2d(object):
+    """Class representing replica and it's associated data.
+    """
+    def __init__(self, my_id, new_temperature=None, new_salt_concentration=None, cores=1):
+        """Constructor.
+
+        """
+        self.id = int(my_id)
+        self.sid = int(my_id)
+        self.cycle = 0
+        if new_salt_concentration is None:
+            self.new_salt_concentration = 0
+        else:
+            self.new_salt_concentration = new_salt_concentration
+        self.old_salt_concentration = new_salt_concentration
+
+        if new_temperature is None:
+            self.new_temperature = 0
+        else:
+            self.new_temperature = new_temperature
+        self.old_temperature = new_temperature
+
+        self.potential = 0
+
+        # amber stuff
+        self.new_traj = ""  # ok
+        self.new_info = ""  # ok
+        self.new_coor = ""  # ok both namd and amber
+
+
+        self.old_traj = ""  # ok
+        self.old_info = ""  # ok 
+        self.old_coor = ""  # ok both namd and amber
+        ###################
+
+        
+        self.new_vel = ""         # namd only
+        self.new_history = ""     # namd only
+        self.new_ext_system = ""  # namd only
+        self.old_vel = ""         # namd only
+        self.old_ext_system = ""  # namd only
+
+
         self.old_path = ""
         self.first_path = ""
         self.swap = 0
