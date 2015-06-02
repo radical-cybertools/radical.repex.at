8577783fe81116d7b156f3d3d4a02ab196783122
AntonsT <antons.treikalis@rutgers.edu>
2015-06-01 21:09:54 -0400

    addint proper versioning

--- setup.py
+++ setup.py
@@ -1,12 +1,30 @@
+import re
+import subprocess
 from distutils.core import setup
 from setuptools import setup, find_packages
 
+def get_version():
+    v_long = subprocess.check_output(['git', 'rev-parse', 'HEAD'])
+    v_short = subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD'])
+
+    VERSIONFILE="src/radical/repex/_version.py"
+    verstrline = open(VERSIONFILE, "rt").read()
+    VSRE = r"^__version__ = ['\"]([^'\"]*)['\"]"
+    mo = re.search(VSRE, verstrline, re.M)
+    if mo:
+        verstr = mo.group(1)
+        verstr = verstr + "@" + v_short
+    else:
+        raise RuntimeError("Unable to find version string in %s." % (VERSIONFILE,))
+    return verstr
+
+#-------------------------------------------------------------------------------
 
 setup(
     name='RepEx',
-    version='0.1',
+    version=get_version(),
     author='Antons Treikalis',
-    author_email='at646@scarletmail.rutgers.edu',
+    author_email='antons.treikalis@rutgers.edu',
     packages=['repex_utils', 
               'repex', 
               'replicas', 
@@ -33,7 +51,7 @@ setup(
                  'amber_kernels_us': 'src/radical/repex/md_kernels/amber_kernels_us',
                  'amber_kernels_3d_tuu': 'src/radical/repex/md_kernels/amber_kernels_3d_tuu',
                  'amber_kernels_3d_tsu': 'src/radical/repex/md_kernels/amber_kernels_3d_tsu'},
-    scripts=[],
+    scripts=['bin/repex-version'],
     license='LICENSE.txt',
     description='Radical Pilot based Replica Exchange Simulations Module',
     long_description=open('README.md').read(),
