368aab8b66c1b07172a77364a1668b29523a3472
Taisung Lee <taisung@gmail.com>
2015-06-10 10:28:36 -0400

    rename input json files

--- examples/amber_pattern_b_3d_tuu/starter.sh
+++ examples/amber_pattern_b_3d_tuu/starter.sh
@@ -1,2 +1,3 @@
 export RADICAL_REPEX_VERBOSE=info; 
-python launch_simulation_pattern_b_3d_tuu.py --input='testing_input.json'
+#python launch_simulation_pattern_b_3d_tuu.py --input='ala_10_input.json'
+python launch_simulation_pattern_b_3d_tuu.py --input='ace_ala_mne_input.json'
