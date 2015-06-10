3a7cc547a97f1e5cf905c1ddceda4301ff9a293c
Taisung Lee <taisung@gmail.com>
2015-06-10 10:33:51 -0400

    typo

--- examples/amber_pattern_b_3d_tuu/starter.sh
+++ examples/amber_pattern_b_3d_tuu/starter.sh
@@ -1,3 +1,3 @@
 export RADICAL_REPEX_VERBOSE=info; 
 #python launch_simulation_pattern_b_3d_tuu.py --input='ala_10_input.json'
-python launch_simulation_pattern_b_3d_tuu.py --input='ace_ala_mne_input.json'
+python launch_simulation_pattern_b_3d_tuu.py --input='ace_ala_nme_input.json'
