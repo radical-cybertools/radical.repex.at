3f6efcd8adb216cb9bed17cb0500ef5615902d2e
Haoyuan Chen <haoyuan.chen@rutgers.edu>
2015-07-07 13:15:28 -0400

    check in the phosphoryl transfer qm/mm example

--- examples/amber_pattern_b_3d_tuu/starter.sh
+++ examples/amber_pattern_b_3d_tuu/starter.sh
@@ -4,4 +4,5 @@ export RADICAL_REPEX_VERBOSE=info;
 #export SAGA_VERBOSE=debug;
 #export RADICAL_VERBOSE=debug; 
 #python launch_simulation_pattern_b_3d_tuu.py --input='test_local_input.json'
+#python launch_simulation_pattern_b_3d_tuu.py --input='phos_trans_input.json'
 python launch_simulation_pattern_b_3d_tuu.py --input='ace_ala_nme_input.json'
