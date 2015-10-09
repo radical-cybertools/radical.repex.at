55c40885dfc4f199cf083b88c3e64f2377631d7e
Haoyuan Chen <haoyuan.chen@rutgers.edu>
2015-10-09 19:25:25 -0400

    add the DNA helix junction system for TSU

--- examples/amber_pattern_b_3d_tsu/starter_3d.sh
+++ examples/amber_pattern_b_3d_tsu/starter_3d.sh
@@ -1,12 +1,12 @@
 export RADICAL_REPEX_VERBOSE=info;
-export RADICAL_PILOT_VERBOSE=info;
+#export RADICAL_PILOT_VERBOSE=info;
 
 #export RADICAL_PILOT_VERBOSE=debug;
 #export SAGA_VERBOSE=debug;
 #export RADICAL_VERBOSE=debug; 
 
-python launch_simulation_pattern_b_3d_tsu.py --input='ace_ala_nme_input.json'
-
+#python launch_simulation_pattern_b_3d_tsu.py --input='ace_ala_nme_input.json'
+python launch_simulation_pattern_b_3d_tsu.py --input='dna_helix_jct_input.json'
 #python launch_simulation_pattern_b_3d_tsu.py --input='input_local.json'
 
 #python launch_simulation_pattern_b_3d_tsu.py --input='local.json'
