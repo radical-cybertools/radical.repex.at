fbe03d07ea56ad62b832839fb8f5cb49092cf68d
AntonsT <antons.treikalis@rutgers.edu>
2016-02-09 12:42:36 -0500

    correct db url

--- src/radical/repex/pilot_kernels/pilot_kernel.py
+++ src/radical/repex/pilot_kernels/pilot_kernel.py
@@ -48,7 +48,7 @@ class PilotKernel(object):
 
         if self.dburl is None:
             self.logger.info("Using default Mongo DB url")
-            self.dburl = "mongodb://treikali:pf43ek6klo@ds047095.mongolab.com:47095/repex-tests"
+            self.dburl = "mongodb://treikali:pf43ek6klo@ds051595.mongolab.com:51595/cdi-testing"
 
         cleanup = rconfig['target'].get('cleanup','False')
         if (cleanup == "True"):
