d1a84c9af074c00d984799cffcd397f26074db68
AntonsT <antons.treikalis@rutgers.edu>
2016-04-20 18:14:37 -0400

    error handling...

--- examples/amber/run.sh
+++ examples/amber/run.sh
@@ -2,5 +2,5 @@ export RADICAL_REPEX_VERBOSE=info;
 export RADICAL_PILOT_VERBOSE=info;
 export REPEX_PROFILING=1;
 
-repex-amber --input='t_remd_ala10.json' --rconfig='comet.json'
+repex-amber --input='t_remd_ala10.json' --rconfig='local.json'
 
