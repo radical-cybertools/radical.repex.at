78fb8521a1c9c1145d59ee063317d2e0b0794461
Antons <at646@scarletmail.rutgers.edu>
2016-07-20 14:49:47 -0400

    Update run.sh

--- examples/amber/run.sh
+++ examples/amber/run.sh
@@ -1,9 +1,9 @@
 export RADICAL_REPEX_VERBOSE=INFO;
 export RADICAL_PILOT_VERBOSE=INFO;
 export SAGA_VERBOSE=INFO;
-#export RADICAL_VERBOSE=DEBUG; 
 export REPEX_PROFILING=1;
 export RADICAL_PILOT_PROFILE=TRUE;
+export SAGA_PTY_SSH_TIMEOUT=500;
 
 nohup repex-amber --input='tuu_remd_phos_trans_qmmm.json' --rconfig='stampede.json' 2> stamp-qmmm-async-r216-c240.log &
 
