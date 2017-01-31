08f4c6f3b1adc4e1145f4382d444c2c592489673
AntonsT <antons.treikalis@rutgers.edu>
2017-01-31 00:57:36 -0500

    cleanup 3

--- examples/amber/run.sh
+++
@@ -1,7 +0,0 @@
-export RADICAL_PILOT_VERBOSE=INFO;
-export SAGA_VERBOSE=INFO;
-export RADICAL_PILOT_PROFILE=TRUE;
-export SAGA_PTY_SSH_TIMEOUT=1000;
-
-nohup repex-amber --input='us_remd_ace_ala_nme.json' --rconfig='local.json' 2> local.log &
-
