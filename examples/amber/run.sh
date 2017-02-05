3263e85eb36e7c6fcbbee953208521e1987cde9c
AntonsT <antons.treikalis@rutgers.edu>
2017-02-04 20:35:43 -0500

    fixes

--- examples/amber/run.sh
+++ examples/amber/run.sh
@@ -1,8 +1,5 @@
 export RADICAL_REPEX_VERBOSE=info;
 export RADICAL_PILOT_VERBOSE=debug;
 
-#python launch_simulation_pattern_b.py --input='amber_input.json'
-
 repex-amber --input='amber_input.json' --rconfig='stampede.json'
 
-#nohup python launch_simulation_pattern_b.py --input='amber_input.json' 2> log.1 &
